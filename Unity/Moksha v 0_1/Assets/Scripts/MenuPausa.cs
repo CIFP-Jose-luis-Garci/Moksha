22c31708b364ffeda3848d8cbdc3bb8998b69393
Martix-art <leo.enc.marta@gmail.com>
2021-01-20 12:25:05 +0100

    UI
    Realizado Menu Pausa y Menu Controles, no consigo que el menu controles al darle al boton de play vuelva al juego

---
+++ Unity/Moksha v 0_1/Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    public void PantallaIncio ()
+    {
+        SceneManager.LoadScene("Menu_Inicio");
+    }
+    public void Controles()
+    {
+        SceneManager.LoadScene("Menu_Controles");
+    }
+
+    public void Play ()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
+
