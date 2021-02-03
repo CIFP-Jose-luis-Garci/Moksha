e574a6f93fc0c3967a97bb074f2f27ce32e68d7f
Martix-art <leo.enc.mart@gmail.com>
2021-02-03 20:41:01 +0100

    UI
    Pantalla de inicio le funcionan los botones para moverlo con el mando, la escala esta corregido y esta trampeada la pantalla de carga

--- Unity/Moksha v 0_1/Assets/Scripts/CargadorNivel.cs
+++ Unity/Moksha v 0_1/Assets/Scripts/CargadorNivel.cs
@@ -9,25 +9,50 @@ public class CargadorNivel : MonoBehaviour
     public GameObject PantallaDeCarga;
     public Slider Slider;
 
-   public void CargarNivel(int NumeroDeEscena)
+    public void CargarNivel(int NumeroDeEscena)
     {
         StartCoroutine(CargarAsync(NumeroDeEscena));
 
     }
-
     IEnumerator CargarAsync(int NumeroDeEscena)
     {
-        AsyncOperation Operacion = SceneManager.LoadSceneAsync(NumeroDeEscena);
+       // AsyncOperation Operacion = SceneManager.LoadSceneAsync(NumeroDeEscena);
 
         PantallaDeCarga.SetActive(true);
 
-        while (!Operacion.isDone)
-        {
-            float Progreso = Mathf.Clamp01(Operacion.progress / .9f);
-            Slider.value = Progreso;
+        float Progreso = 0f;
 
+        while (Progreso <=1)
+        {
+            Slider.value = Progreso;
+            Progreso += 0.01f;
+            
+            if(Progreso > 0.99)
+            {
+                SceneManager.LoadSceneAsync(NumeroDeEscena);
+            }
+            
+            
             yield return null;
         }
 
     }
+    
+    /*
+   IEnumerator CargarAsync(int NumeroDeEscena)
+   {
+       AsyncOperation Operacion = SceneManager.LoadSceneAsync(NumeroDeEscena);
+
+       PantallaDeCarga.SetActive(true);
+
+       while (!Operacion.isDone)
+       {
+           float Progreso = Mathf.Clamp01(Operacion.progress / .9f);
+           Slider.value = Progreso;
+
+           yield return null;
+       }
+
+   }
+   */
 }
