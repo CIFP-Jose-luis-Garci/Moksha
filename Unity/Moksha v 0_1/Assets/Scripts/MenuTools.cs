867006b9d3c8bb2ce65d4a1da8a3958116facc7c
Álvaro Holguera <correo@laultimapregunta.com>
2021-02-19 20:07:53 +0100

    Menú animado
    En la escena de Pruebas Scripts he creado un menú animado. Se lanza con la barra espaciadora, y el botón de la Burbuja activa una escafandra que he añadido como prefab

---
+++ Unity/Moksha v 0_1/Assets/Scripts/MenuTools.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuTools : MonoBehaviour
+{
+
+    Animator anim;
+    [SerializeField] GameObject escafandra;
+
+    bool menuHidden = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GameObject.Find("MenuTools").GetComponent<Animator>();
+
+        escafandra.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Si pulsamos la tecla de herramientas
+        if(Input.GetKeyDown("space"))
+        {
+            //Si el menú está oculto, lo mostramos
+            if(menuHidden)
+            {
+                anim.SetTrigger("Show_menu");
+                menuHidden = false;
+                
+            }
+            else
+            {
+                anim.SetTrigger("Hide_menu");
+                menuHidden = true;
+            }
+        }
+    }
+
+    public void PonerEscafandra()
+    {
+        //Si no lleva la escafandra, se la ponemos, y al revés
+        if(escafandra.activeSelf == false)
+        {
+            escafandra.SetActive(true);
+        }
+        else
+        {
+            escafandra.SetActive(false);
+        }
+        
+    }
+}
