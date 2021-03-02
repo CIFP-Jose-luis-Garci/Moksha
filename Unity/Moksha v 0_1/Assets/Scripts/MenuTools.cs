27f91d7eecc90a9216fa017f3fd26373b2f4b4fb
Alvaro Holguera <correo@laultimapregunta.com>
2021-03-02 18:35:01 +0100

    Botones para navegar por el menú de herramientas
    IMPORTANTE: he añadido un nuevo eje, y sobre todo, no he querido sincronizar la escena, así que para que funcione tenéis que seleccionar InitMeny y arrastrar las herramientas al array que hay creado

--- Unity/Moksha v 0_1/Assets/Scripts/MenuTools.cs
+++ Unity/Moksha v 0_1/Assets/Scripts/MenuTools.cs
@@ -11,17 +11,31 @@ public class MenuTools : MonoBehaviour
 
     bool menuHidden = true;
 
+    //Flechas del mando
+    float padArrows;
+
+    //Array con los botones
+    [SerializeField] Button[] botonesArray = new Button[6];
+    int buttonSelected = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         anim = GameObject.Find("MenuTools").GetComponent<Animator>();
 
         escafandra.SetActive(false);
+
+        //Obtenemos los botones del menú y los metemos en el array
+        //botonesArray = GameObject.Find("MenuTools").GetComponentsInChildren<Button>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //Flechas del mando
+        padArrows = Input.GetAxis("HerramientasNav");
+        
         //Si pulsamos la tecla de herramientas
         if(Input.GetButtonDown("Herramientas"))
         {
@@ -30,12 +44,13 @@ public class MenuTools : MonoBehaviour
             {
                 anim.SetTrigger("Show_menu");
                 menuHidden = false;
-                
+                StartCoroutine("MenuNavigation");
             }
             else
             {
                 anim.SetTrigger("Hide_menu");
                 menuHidden = true;
+                StopCoroutine("MenuNavigation");
             }
         }
     }
@@ -46,11 +61,49 @@ public class MenuTools : MonoBehaviour
         if(escafandra.activeSelf == false)
         {
             escafandra.SetActive(true);
+            
         }
         else
         {
             escafandra.SetActive(false);
+            
         }
         
+    }
+
+    //Corrutina que comprueba cada segundo si estamos pulsando las felchas
+    IEnumerator MenuNavigation()
+    {
+        //Seleccionamos el botón correspondiente
+        botonesArray[buttonSelected].Select();
+
+        while(true)
+        {
+            
+            //print(padArrows);
+            if(padArrows > 0.5f)
+            {
+                //Si no hemos llegado al último, seleccionamos el siguiente botón
+                if(buttonSelected < 5)
+                {
+                    buttonSelected++;
+                    botonesArray[buttonSelected].Select();
+                    //print("derecha");
+                }
+                
+            }
+            else if(padArrows < -0.5f)
+            {
+                //Si no hemos llegado al último, seleccionamos el anterior botón
+                if (buttonSelected > 0)
+                {
+                    buttonSelected--;
+                    botonesArray[buttonSelected].Select();
+                    //print("izquierda");
+                }
+            }
+   
+            yield return new WaitForSeconds(0.15f);
+        }
     }
 }
